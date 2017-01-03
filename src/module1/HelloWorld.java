1cc5356d226e228f1e89dac723e76597b5cb438b
Kaustubh Joshi <kjoshi@golden-technology.in>
2017-01-03 16:39:11 +0530

    assignment 1. map 2

--- src/module1/HelloWorld.java
+++ src/module1/HelloWorld.java
@@ -10,9 +10,8 @@ import de.fhpotsdam.unfolding.utils.MapUtils;
 
 /** HelloWorld
   * An application with two maps side-by-side zoomed in on different locations.
-  * Author: UC San Diego Coursera Intermediate Programming team
-  * @author Your name here
-  * Date: July 17, 2015
+  * @author Kaustubh Joshi
+  * Date: January 3, 2017
   * */
 public class HelloWorld extends PApplet
 {
@@ -76,6 +75,10 @@ public class HelloWorld extends PApplet
 		
 		// TODO: Add code here that creates map2 
 		// Then you'll modify draw() below
+		
+		map2 = new UnfoldingMap(this, 450, 50, 350, 500, provider);
+		map2.zoomAndPanTo(zoomLevel, new Location(28.610804f, 77.385557f));
+		MapUtils.createDefaultEventDispatcher(this, map2);
 
 	}
 
@@ -84,6 +87,7 @@ public class HelloWorld extends PApplet
 		// So far we only draw map1...
 		// TODO: Add code so that both maps are displayed
 		map1.draw();
+		map2.draw();
 	}
 
 	
