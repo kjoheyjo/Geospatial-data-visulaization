00b9af29f34bc32eedd824f19c575609f087e022
Kaustubh Joshi <kjoshi@golden-technology.in>
2017-01-24 10:53:35 +0530

    module 1

--- src/module1/HelloWorld.java
+++ src/module1/HelloWorld.java
@@ -64,7 +64,7 @@ public class HelloWorld extends PApplet
 		// The 6th argument specifies the map provider.  
 		// There are several providers built-in.
 		// Note if you are working offline you must use the MBTilesMapProvider
-		map1 = new UnfoldingMap(this, 50, 50, 350, 500, provider);
+		map1 = new UnfoldingMap(this, 50, 50, 300, 500, provider);
 
 		// The next line zooms in and centers the map at 
 	    // 32.9 (latitude) and -117.2 (longitude)
@@ -76,7 +76,7 @@ public class HelloWorld extends PApplet
 		// TODO: Add code here that creates map2 
 		// Then you'll modify draw() below
 		
-		map2 = new UnfoldingMap(this, 450, 50, 350, 500, provider);
+		map2 = new UnfoldingMap(this, 450, 50, 300, 500, provider);
 		map2.zoomAndPanTo(zoomLevel, new Location(28.610804f, 77.385557f));
 		MapUtils.createDefaultEventDispatcher(this, map2);
 
